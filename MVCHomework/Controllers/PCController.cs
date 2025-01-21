ca8c1f0320beda7e3041b8f500d3c1292708a6a3
FernandoFonseca-code <fernando.fonseca2021@gmail.com>
2025-01-21 13:45:18 -0800

    Add BuiltPC model and create actions for PC and Laptop
    - Added BuiltPC model class in BuiltPC.cs
    - Added Create action in PCController.cs with HttpGet and HttpPost methods for creating a new PC build.
    - Added CreateLaptop action in PCController.cs with HttpGet method for creating a new laptop build.
    - Updated Create.cshtml to include form fields for building a PC.
    - Added CreateLaptop.cshtml view for building a laptop using the BuiltPC model.
    - Updated _Layout.cshtml to include a navigation link for "Build a Laptop".

--- MVCHomework/Controllers/PCController.cs
+++ MVCHomework/Controllers/PCController.cs
@@ -1,10 +1,43 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MVCHomework.Models;
 
 namespace MVCHomework.Controllers
 {
     public class PCController : Controller
     {
+        [HttpGet] // GET: /PC/Create
         public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost] // This Create action is hit after the user submits the form
+        public IActionResult Create(IFormCollection BuildFormData)
+        {
+            // validate all data
+
+            BuiltPC newBuild = new()
+            {
+                Title = BuildFormData["BuildTitle"],
+                Type = BuildFormData["BuildType"],
+                CPU = BuildFormData["CPU"],
+                GPU = BuildFormData["GPU"],
+                Budget = Convert.ToDouble(BuildFormData["Budget"])
+            };
+
+
+            // Add to database
+            // BuiltPCDb.Add(newBuild);
+
+            // Add modal dialog box confirming information
+            // Message.Show("Build Created", "Your build has been created successfully", "success");
+
+            // Return a view
+            return RedirectToAction("Index", "Home");
+        }
+
+        [HttpGet] // 
+        public IActionResult CreateLaptop()
+
         {
             return View();
         }
---
+++ MVCHomework/Models/BuiltPC.cs
@@ -0,0 +1,33 @@
+﻿namespace MVCHomework.Models
+{
+    /// <summary>
+    /// Represents a user built PC item
+    /// </summary>
+    public class BuiltPC
+    {
+        /// <summary>
+        /// Represents the title of the users build
+        /// </summary>
+        public string Title { get; set; }
+
+        /// <summary>  
+        /// The type of build the user is creating
+        /// </summary>
+        public string Type { get; set; }
+        /// <summary>
+        /// Represents the chosen CPU of the user's build
+        /// </summary>
+        public string CPU { get; set; }
+        /// <summary>
+        /// Represents the chosen GPU of the user's build
+        /// </summary>
+        public string GPU { get; set; }
+        /// <summary>
+        /// User inputted budget
+        /// </summary>
+        public double Budget { get; set; }
+
+
+
+    }
+}
