982fc43b97926d5f2e3f28e40974a713a589ed2d
FernandoFonseca-code <fernando.fonseca2021@gmail.com>
2025-01-21 11:11:26 -0800

    Add project files.

---
+++ MVCHomework/Controllers/PCController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVCHomework.Controllers
+{
+    public class PCController : Controller
+    {
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
